c3b5c996103e0c58c1851fa2f0bfc745a89f248c
Aquib Faiyaz <faiyazaquib28@gmail.com>
2021-10-24 10:24:23 +0530

    Created bot

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
+const Telegraf = require("telegraf");
+const axios = require("axios");
+const request = require("request");
+require("dotenv").config();
+const getRestaurants = require("./utils/getRestaurants");
 
-const Telegraf = require('telegraf');
-const axios = require('axios');
-const request = require('request');
-const getRestaurants = require('./utils/getRestaurants');
+const code = process.env.BOT_URI;
+const bot = new Telegraf(code);
 
-
-const bot = new Telegraf('1233420128:AAHliiaWVLSev3N41YW__l4Y8KPvcVP25U8');
-
-
-bot.start(ctx =>{
+bot.start((ctx) => {
   ctx.reply(`Hello ${ctx.from.first_name}`);
-  ctx.reply('You can search restaurants in your location.\neg: type `saket, delhi`')
-})
+  ctx.reply(
+    "You can search restaurants in your location.\neg: type `saket, delhi`"
+  );
+});
 
-bot.on('text', ctx => {
-    
-    const city = ctx.message.text;
-  
-    
-    getRestaurants(city, (error, data) =>{
-      if(error){
-        return ctx.reply('Sorry this loction is not in my directory!! Please try another one')
-      }
-        ctx.reply('Name: '+ data.name + '\n\nLocation: '+data.locate+ '\n\nRating: '+data.rating+ '\n\nMenu-link: '+data.menuLink)
-    })
+bot.on("text", (ctx) => {
+  const city = ctx.message.text;
+
+  getRestaurants(city, (error, data) => {
+    if (error) {
+      return ctx.reply(
+        "Sorry this loction is not in my directory!! Please try another one"
+      );
+    }
+    ctx.reply(
+      "Name: " +
+        data.name +
+        "\n\nLocation: " +
+        data.locate +
+        "\n\nRating: " +
+        data.rating +
+        "\n\nMenu-link: " +
+        data.menuLink
+    );
   });
+});
 
-process.env.PORT
-
-
-
-
-
+bot.launch();
--- utils/getRestaurants.js
+++ utils/getRestaurants.js
@@ -1,42 +1,39 @@
-const request = require('request');
-const axios = require('axios');
+const request = require("request");
+const axios = require("axios");
 
-const getRestaurants = (address ,callback) =>{
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiYXF1aWI0NTYiLCJhIjoiY2tkdG54dGM5MDVjMzJybno4czI3bXptbSJ9.svK5JpvP8wtOGR0JmSxkWg&limit=1`;
-    request({url, json:true}, (error, response) =>{
-        if(error){
-            return callback('Please check your connectivity', undefined);
-        }
-        else if(response.body.features.length===0){
-            return callback('Please enter a correct location', undefined);
-        }
-        else{
-            const latitude = response.body.features[0].center[1];
-            const longitude = response.body.features[0].center[0];
-            
-            axios({
-                method: "GET",
-                url: `https://developers.zomato.com/api/v2.1/geocode?lat=${latitude}&lon=${longitude}`,
-                headers: {
-                  "user-key": "e817e176add64cb95a322155317ba478",
-                  "content-type": "application/json"
-                }
-              })
-                .then(response => {
-                  const resList = response.data.nearby_restaurants;
-                  resList.forEach(dhaba =>{
-                  const name = dhaba.restaurant.name;
-                  const locate= dhaba.restaurant.location.address;
-                  const rating = dhaba.restaurant.user_rating.aggregate_rating;
+const getRestaurants = (address, callback) => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=pk.eyJ1IjoiYXF1aWI0NTYiLCJhIjoiY2tkdG54dGM5MDVjMzJybno4czI3bXptbSJ9.svK5JpvP8wtOGR0JmSxkWg&limit=1`;
+  request({ url, json: true }, (error, response) => {
+    if (error) {
+      return callback("Please check your connectivity", undefined);
+    } else if (response.body.features.length === 0) {
+      return callback("Please enter a correct location", undefined);
+    } else {
+      const latitude = response.body.features[0].center[1];
+      const longitude = response.body.features[0].center[0];
 
-                  const menuLink =  dhaba.restaurant.menu_url;
-                  callback(undefined, {name, locate, menuLink, rating});
-                })
-                
-                });
-        }
-    })
-}
+      axios({
+        method: "GET",
+        url: `https://developers.zomato.com/api/v2.1/geocode?lat=${latitude}&lon=${longitude}`,
+        headers: {
+          "user-key": "e817e176add64cb95a322155317ba478",
+          "content-type": "application/json",
+        },
+      }).then((response) => {
+        const resList = response.data.nearby_restaurants;
+        resList.forEach((restaurant) => {
+          const name = restaurant.restaurant.name;
+          const locate = restaurant.restaurant.location.address;
+          const rating = restaurant.restaurant.user_rating.aggregate_rating;
 
+          const menuLink = restaurant.restaurant.menu_url;
+          callback(undefined, { name, locate, menuLink, rating });
+        });
+      });
+    }
+  });
+};
 
-module.exports = getRestaurants
+module.exports = getRestaurants;
